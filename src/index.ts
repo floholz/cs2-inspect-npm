84c2f972ae829818cbc20b1c0ee82f49eacaa630
floholz <holzmann96flo@gmail.com>
2023-12-17 17:54:32 +0100

    ts init

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export function add(a: number, b: number): number {
+    return a + b;
+}
+
+console.log(add(3, 5)); //output: 8
+
+
+export function main(itemId: number, paintIndex: number, patternSeed: number, paintWear: number) {
+    // !gen 61 1040 1 0.001 0 0 0 0 0 0 0 0
+    // steam://rungame/730/76561202255233023/+csgo_econ_action_preview 00183D209008280038EFA48CD40340018052692E
+
+}
