b94606e72b78ebd49e3918490fb0676da1ad2511
floholz <holzmann96flo@gmail.com>
2023-12-17 20:24:53 +0100

    > added ak 661 test

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,13 @@
 import {CEconItemPreviewDataBlock, CEconItemPreviewDataBlock_Sticker} from "./econ";
 import CRC32 from "crc-32";
-import * as buffer from "buffer";
-
-export function add(a: number, b: number): number {
-    return a + b;
-}
-
-console.log(add(3, 5)); //output: 8
 
 
-export function main(defIndex: number = 61, paintIndex: number = 1040, patternSeed: number = 1, paintWear: number = 0.001): string {
+export function main(defIndex: number = 61, paintIndex: number = 1040, patternSeed: number = 1, paintWear: number = 0.001, rarity: number = 0): string {
     // !gen 61 1040 1 0.001 0 0 0 0 0 0 0 0
     // steam://rungame/730/76561202255233023/+csgo_econ_action_preview 00183D209008280038EFA48CD40340018052692E
 
     const econ: CEconItemPreviewDataBlock = {
-        rarity: 0,
+        rarity: rarity,
         defindex: defIndex,
         paintindex: paintIndex,
         paintseed: patternSeed,
--- tests/gen.test.ts
+++ tests/gen.test.ts
@@ -1,6 +1,16 @@
 import { main } from '../src';
 
-test('adds two numbers correctly', () => {
+test('USP-S The Traitor', () => {
+    // !gen 61 1040 1 0.001 0 0 0 0 0 0 0 0
+    // steam://rungame/730/76561202255233023/+csgo_econ_action_preview 00183D209008280038EFA48CD40340018052692E
     const result = main(61, 1040, 1, 0.001);
     expect(result).toBe("00183D209008280038EFA48CD40340018052692E");
+});
+
+test('AK-47 Case Hardened', () => {
+    // !gen 7 44 661 0.25 0 0 0 0 0 0 0 0
+    // steam://rungame/730/76561202255233023/+csgo_econ_action_preview 001807202C280538808080F403409505480050B90A5A0367656D9A98C3B8
+    // steam://rungame/730/76561202255233023/+csgo_econ_action_preview 001807202C280538808080F403409505CCBBC5DC
+    const result = main(7, 44, 661, 0.25, 5);
+    expect(result).toBe("001807202C280538808080F403409505CCBBC5DC");
 });
